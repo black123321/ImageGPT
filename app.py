9ee6f5b10cd35eee5ac61d0f9dc0ae4aaad746fe
black123321 <121915971+black123321@users.noreply.github.com>
2023-08-17 10:47:31 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import Flask,render_template,request
+import os
+
+app = Flask(__name__)
+
+import openai
+
+openai.api_key = "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"
+
+@app.route("/",methods=["GET","POST"])
+
+def index():
+    if request.method == "POST":
+        t = request.form.get("txt")
+        response = openai.Image.create(prompt=t,n=1)
+        image_url = response['data'][0]['url']
+        print(image_url)
+        return(render_template("index.html",result=image_url))
+    else:
+        return(render_template("index.html",result="waiting"))
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
